e5cacf72fbb79d57b9707e944b25f36dc684a923
Wayne Banks <thestorerun@yahoo.com>
2016-05-04 11:55:04 -0700

    class assignment

--- starter-code/independent-practice.js
+++ starter-code/independent-practice.js
@@ -9,14 +9,14 @@
  * Question 1
  * Create an array of image sources. Use image1.png, image2.png, and image3.png as the array values.
  */
-
+q1 = var imageSourceArray = ["image1.png", "image2.png", "image3.png"]
 // Your code here
 
 /**
  * Question 2
  * Using the array from Question 1, store the first element of the array in variable q2.
  */
-
+var q2 = imageSourceArray[0];
 // Your code here
 
 /**
@@ -25,12 +25,14 @@
  */
 
 // Your code here
+q3 = imageSourceArray.length;
+console.console.log(imageSourceArray);
 
 /**
  * Question 4
  *  Using the array from Question 1, store the last element of the array in variable q4. Hint: How can we get the number of elements in the array?
  */
-
+q4 = imageSourceArray[imageSourceArray.length-1]
 
 // Your code here
 
@@ -49,6 +51,12 @@
  * Create an array of numbers using 1,2,3, and 4 as values. Use forEach to increase each value by 1 and store the value back in the array.
 The end result should be an array of numbers with values 2,3,4,5
  */
+var numbers = [1, 2, 3, 4];
+var forEach = 0;
+if (length.numbers > i) {
+  for (var i = 0; i < 4; i++) {
+  return forEach ++ numbers[i] << 0; 
+}
 
 // Your code here
 
